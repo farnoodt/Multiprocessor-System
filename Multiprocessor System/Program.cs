6c2aafe1933e6edb8dbeafe7004fbd240dc98d87
Farnoud Taghdissi Borhani <Farnood_t@yahoo.com>
2021-09-26 19:43:44 -0700

    Add project files.

---
+++ Multiprocessor System/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiprocessor_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> L = new List<int> { 1, 2, 1, 3, 1, 2 };
+            Console.WriteLine(Multiprocessor_System(6,L,2));
+        }
+
+        static int Multiprocessor_System(int nums, List<int> pages, int MaxCacheSize)
+        {
+            Queue<int> q = new Queue<int>();
+            int count = 0;
+            for (int i = 0; i < nums; i++)
+            {
+                if (!q.Contains(pages[i]))
+                    count++;
+                if (q.Count == MaxCacheSize)
+                    q.Dequeue();
+               
+                q.Enqueue(pages[i]);
+            }
+            return count;
+        }
+    }
+}
